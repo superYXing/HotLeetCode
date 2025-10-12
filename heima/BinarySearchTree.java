df9d1d16eba32dbb885a06e7a9f10e997c87b3c6
superYXing <370116601@mail.dlut.edu.cn>
2025-10-12 10:44:14 +0100

    二叉搜索树的min/max/put

--- heima/BinarySearchTree.java
+++ heima/BinarySearchTree.java
@@ -34,20 +34,68 @@ public class BinarySearchTree {
         while(curr!=null){
             if(curr.key>key) curr = curr.left;
             else if (curr.key<key) curr = curr.right;
-            else break;
+            else return curr.value;
         }
-        if(curr == null) return null;
-        else return curr.value;
+        return null;
+        
+    }
+    public void put(int key,Object value){
+        if(root!=null) {
+        BSTNode curr = root;
+        BSTNode parent = new BSTNode();
+        while(curr!=null){
+            parent = curr;
+            if(curr.key>key) curr = curr.left;
+            else if (curr.key<key) curr = curr.right;
+            else {
+                curr.value = value;
+                return;
+            }
+        }
+        if(key>parent.key) parent.right = new BSTNode(key,value);
+        else parent.left = new BSTNode(key,value);
+    }else{
+        BSTNode n = new BSTNode(key,value);
+        root = n;
+    }
+    return;
+    }
+    public Object min(){
+        if(root == null) return null;
+        BSTNode node = root;
+        while(node.left!=null){
+            node = node.left;
+        }
+        return node.value;
     }
 
+    public Object max(){
+        if(root == null) return null;
+        BSTNode node = root;
+        while(node.right!=null){
+            node = node.right;
+        }
+        return node.value;
+    }
     public BinarySearchTree(){
         root = new BSTNode(4,4,new BSTNode(2,2,new BSTNode(1,1,null,null),new BSTNode(3,3,null,null
-        )),new BSTNode(7,7,new BSTNode(6,6,new BSTNode(5,5,null,null),null),new BSTNode(8,8,null,null)));
+        )),new BSTNode(7,56,new BSTNode(6,6,new BSTNode(5,5,null,null),null),new BSTNode(8,8,null,null)));
     }
+
 
     public static void main(String[] args) {
         BinarySearchTree root = new BinarySearchTree();
-        int val = (int)root.get(10);
-        System.out.println(val);
+//         Object val = root.get(72);
+//         // 先判断是否为null，是则打印null
+// if (val == null) {
+//     System.out.println(val);
+// } 
+// // 非null时判断是否为Integer类型，是则输出
+// else if (val instanceof Integer) {
+//     System.out.println(val);
+// }
+    root.put(4, 100);
+    int val = (int)root.get(4);
+    System.out.println(val);
     }
 }
